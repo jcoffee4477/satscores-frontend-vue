05c08de643181a701edf8072c65ea43547e625a0
jcoffee4477 <jacobcoffee@jacobs-air.mynetworksettings.com>
2023-09-19 16:42:44 -0400

    user can view math/enlish scores for schools

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <input type="text" value = {{searchFilter}} onChange={{(event) => searchFilter = event.target.value}}/> -->
+    <h1>{{ message }}</h1>
+    <button v-on:click="sortedSchools">button</button>
+    <div v-for="school in schools">
+      <p>{{ school[9] }}</p>
+      <button v-on:click="showSchool(school)">More Info </button>
+      <hr />
+    </div>
+    <dialog id="school-details" >
+    <form method="dialog">
+      <p> Avgerage math score: {{ currentSchool[12] }}</p>
+      <p> Average enlgish score: {{ currentSchool[13] }}</p>
+      <button>Close</button>
+    </form>
+    </dialog>
   </div>
 </template>
+<style>
+</style>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      schools: [],
+      searchFilter: "",
+      currentSchool: {},
+      sortedSchools: [],
+    };
+  },
+  created: function() {
+    this.getData()
+  },
+  methods: {
+    getData: function() {
+      console.log('get data')
+      axios.get("https://data.cityofnewyork.us/api/views/f9bf-2cp4/rows.json").then(response => {
+        console.log(response.data)
+        this.schools = response.data.data
+      })
+    },
+    showSchool: function(school) {
+      this.currentSchool = school
+      document.querySelector("#school-details").showModal();
+    },
+    
   }
 }
+  
+;
 </script>
